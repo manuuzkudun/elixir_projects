14b979377679f00940cbb288067b02d6096896e9
Manu Uzcudun <manu.uzcudun@wuaki.tv>
2018-01-18 18:27:59 +0100

    Authentication and authorization works

---
+++ discuss/web/models/user.ex
@@ -0,0 +1,21 @@
+defmodule Discuss.User do
+  use Discuss.Web, :model
+
+  # @derive {Poison.Encoder, only: [:email]}
+
+  schema "users" do
+    field :email, :string
+    field :provider, :string
+    field :token, :string
+    has_many :topics, Discuss.Topic
+    # has_many :comments, Discuss.Comment
+
+    timestamps()
+  end
+
+  def changeset(struct, params \\ %{}) do
+    struct
+    |> cast(params, ~w(email provider token)a)
+    |> validate_required(~w(email provider token)a)
+  end
+end
