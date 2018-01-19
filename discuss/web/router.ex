cf7cd1e4a525d7b1b4ab43f6b086c1b31463802d
Manu Uzcudun <manu.uzcudun@wuaki.tv>
2018-01-19 18:45:35 +0100

    websockets implemented

--- discuss/web/router.ex
+++ discuss/web/router.ex
@@ -17,9 +17,7 @@ defmodule Discuss.Router do
   scope "/", Discuss do
     pipe_through :browser # Use the default browser stack
 
-    get "/", TopicController, :index
-    get "/topics/new", TopicController, :new
-    post "/topics", TopicController, :create
+    resources "/", TopicController
   end
 
   scope "/auth", Discuss do
